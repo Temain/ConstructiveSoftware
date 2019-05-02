d68be20e00e7a6bf8b97ae030e345171be34b62f
temain <asv.artm@gmail.com>
2019-05-02 22:55:13 +0300

    Fix

--- ConstructiveSoftware.WebApi/Startup.cs
+++ ConstructiveSoftware.WebApi/Startup.cs
@@ -68,6 +68,8 @@ namespace ConstructiveSoftware.WebApi
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			app.UseCors("AllowSpecificMethods");
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
